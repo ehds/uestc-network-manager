ec003db721493efce280be20f666c27b47fb4b31
dshe <ehds@qq.com>
2020-11-17 16:57:34 +0800

    add service support (#3)
    Add systemd support

---
+++ unit/uestcnetwork.service.cmake
@@ -0,0 +1,17 @@
+[Unit]
+Description=UESTC Network Login
+Documention=
+After=network.target
+
+[Service]
+Type=${SYSTEMD_SERVICE_TYPE}
+ExecStart=${CMAKE_INSTALL_PREFIX}/bin/uestc_login
+ExecReload=/bin/kill -s USR1 $MAINPID
+WatchdogSec=${WATCHDOG_SEC}
+Restart=always
+User=${SERVICE_USER}
+Group=${SERVICE_GROUP}
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
