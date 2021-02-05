f9b926ee00d0c2bef7f527c88986d3503742d981
ehds <ehds@qq.com>
2021-02-05 23:05:44 +0800

    Remove service user and group

--- unit/uestcnetwork.service.cmake
+++ unit/uestcnetwork.service.cmake
@@ -9,8 +9,6 @@ ExecStart=${CMAKE_INSTALL_PREFIX}/bin/uestcnetwork
 ExecReload=/bin/kill -s USR1 $MAINPID
 WatchdogSec=${WATCHDOG_SEC}
 Restart=always
-User=${SERVICE_USER}
-Group=${SERVICE_GROUP}
 AmbientCapabilities=CAP_NET_BIND_SERVICE
 
 [Install]
